704ebcc1181515f477934f10e1b23950bac8857a
lla-dane <abhinavagarwalla6@gmail.com>
2025-10-26 12:21:08 +0530

    Remove redundants

--- bash/tut/ifelif.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ${1,,} = shelby ]; then
-	echo "Oh, you are the boss here, welcome!"
-elif [ ${1,,} = help ]; then 
-	echo "Just enter the username, duh!"
-else 
-	echo "i dont know who you are but you are not the boss of me!"
-fi
